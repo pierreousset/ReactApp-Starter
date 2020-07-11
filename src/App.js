d60d9f55d879e819f1c754333b662ad0ce5723dd
pierre <pierre.ousset@staffmatch.com>
2020-07-11 08:49:40 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return(
+        <div>
+        My App Component
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
+module.exports = {
+   context: __dirname,
+   entry: './src/index.js',
+   output: {
+      path: path.resolve( __dirname, 'dist' ),
+      filename: 'main.js',
+      publicPath: '/',
+   },
+   devServer: {
+      historyApiFallback: true,
+      contentBase: path.resolve('./public'),
+      port: 3000,
+      stats: 'minimal',
+      quiet: false,
+      noInfo: false,
+      open: true, // Open navigate
+      lazy: false,
+      historyApiFallback: true, // For react-router
+      disableHostCheck: true,
+      hot: true,
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            use: 'babel-loader',
+         },
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+         },
+         {
+            test: /\.(png|j?g|svg|gif)?$/,
+            use: 'file-loader'
+         },
+         {
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+         }    
+     ]
+   },
+   plugins: [
+      new HtmlWebPackPlugin({
+         template: path.resolve( __dirname, 'public/index.html' ),
+         filename: 'index.html'
+      })
+   ]
+};
+
