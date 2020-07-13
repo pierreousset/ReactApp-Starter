aae4f8b2a65a614c1a60f9cef6b8afc0e354ac4b
pierre <pierre.ousset@staffmatch.com>
2020-07-13 23:11:53 +0200

    add eslint and redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { css } from '@emotion/core';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+    const alert = useSelector(state => state.alert);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }, []);
   return(
-    <div css={css`color: red;`}>
-      My App Component
-    </div>
+    <StrictMode>
+      <Router history={history}>
+        <Switch>
+          <PrivateRoute exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    </StrictMode>
   );
 }
 
